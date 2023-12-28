421506d3ec79c729bbc2be0eefc2a75de7ea2c5d
LaylaR <layla.richard.2111@gmail.com>
2023-12-28 15:21:23 +0100

    try environments var config

---
+++ src/environments/set-env.ts
@@ -0,0 +1,28 @@
+﻿const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+// Load node modules
+  const colors = require('colors');
+  require('dotenv').config({
+    path: 'src/environments/.env'
+  });
+// `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  cogipApiKey: '${process.env["COGIP_API_KEY"]}',
+    production: true,
+};
+`;
+  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  writeFile(targetPath, envConfigFile, (err:any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+  });
+};
+
+setEnv();
